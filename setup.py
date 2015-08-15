bf03d2b8e4b3f425bdf5659693bf6b262d206921
Martin Weigert <mweigert@mpi-cbg.de>
2015-08-15 15:26:19 +0200

    eadded deps in setup

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(name='gputools',
     author_email='mweigert@mpi-cbg.de',
     license='MIT',
     packages=['gputools'],
-    install_requires=["pyopencl"],
+    install_requires=["pyopencl, pyfft"],
     package_data={},
     entry_points = {}
 )
