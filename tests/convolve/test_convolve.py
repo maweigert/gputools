07eeee20b2e56ecd7e7e77e1bdbfac01861e10b8
mweigert <mweigert@mpi-cbg.de>
2015-08-31 19:40:30 +0200

    added more reasonable convolve test

--- tests/convolve/test_convolve.py
+++ tests/convolve/test_convolve.py
@@ -5,49 +5,29 @@ import scipy.ndimage.filters as sp_filter
 
 import gputools
 
-
-def test_convolve2():
+def _convolve_rand(dshape,hshape):
+    print "convolving test: dshape = %s, hshape  = %s"%(dshape,hshape)
     np.random.seed(1)
-    N = 256
-    Nh = 11 
-    d = np.random.uniform(-1,1,(N,)*2).astype(np.float32)
-    h = np.random.uniform(-1,1,(Nh,)*2).astype(np.float32)
-    
-    out1 = sp_filter.convolve(d,h,mode="constant")
-
-    out2 = gputools.convolve(d,h)
-
-    npt.assert_allclose(out1,out2,atol=1.e-5)
-
-
-
-def test_convolve1():
-
-    np.random.seed(1)
-    N = 256
-    Nh = 11 
-    d = np.random.uniform(-1,1,(N,)*1).astype(np.float32)
-    h = np.random.uniform(-1,1,(Nh,)*1).astype(np.float32)
-    out1 = sp_filter.convolve(d,h,mode="constant")
-
-    out2 = gputools.convolve(d,h)
-
-    npt.assert_allclose(out1,out2,atol=1.e-5)
-
-    
-if __name__ == '__main__':
-    # test_convolve2()
-
-   
-    np.random.seed(1)
-    N = 64
-    Nh = 7 
-    d = np.random.uniform(-1,1,(N,)*3).astype(np.float32)
-    h = np.random.uniform(-1,1,(Nh,)*3).astype(np.float32)
+    d = np.random.uniform(-1,1,dshape).astype(np.float32)
+    h = np.random.uniform(-1,1,hshape).astype(np.float32)
     
     out1 = sp_filter.convolve(d,h,mode="constant")
 
     out2 = gputools.convolve(d,h)
 
     npt.assert_allclose(out1,out2,rtol=1.e-2,atol=1.e-5)
+
+def test_convolve():
+    for ndim in [1,2,3]:
+        for N in range(10,200,40):
+            for Nh in range(3,11,2):
+                dshape = [N/ndim+3*n for n in range(ndim)]
+                hshape = [Nh+3*n for n in range(ndim)]
+                
+                _convolve_rand(dshape,hshape)
+    
+if __name__ == '__main__':
+    test_convolve()
+
+   
 
