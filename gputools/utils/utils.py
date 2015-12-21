31301b62c7e0d4b779dbed7a83a452ca691a066f
Martin Weigert <mweigert@mpi-cbg.de>
2015-12-21 16:40:03 +0100

    added function to remove cache

--- gputools/utils/utils.py
+++ gputools/utils/utils.py
@@ -23,3 +23,27 @@ def pad_to_power2(data, mode="constant"):
         return data
     else:
         return pad_to_shape(data,[_next_power_of_2(n) for n in data.shape],mode)
+
+
+def get_cache_dir():
+    from tempfile import gettempdir
+    import getpass
+    import os
+    import sys
+
+    return os.path.join(gettempdir(),
+                    "pyopencl-compiler-cache-v2-uid%s-py%s" % (
+                        getpass.getuser(), ".".join(str(i) for i in sys.version_info)))
+
+def remove_cache_dir():
+    import shutil
+
+    cache_dir = get_cache_dir()
+    print("try removing cache dir: %s"%cache_dir)
+    try:
+        shutil.rmtree(cache_dir)
+    except Exception as e:
+        print(e)
+
+
+
