4fe317e92b66346285a69695700c0302dbf3fb54
Martin Weigert <mweigert@mpi-cbg.de>
2016-08-11 18:16:22 +0200

    minor

--- build_pypi.sh
+++ build_pypi.sh
@@ -1,6 +1,11 @@
+#clean up old dists
+rm dist/*
+
+
 python setup.py sdist bdist_wheel
+
+
 # python setup.py register -r pypitest
 # twine upload -r pypitest dist/gputools*
-
 python setup.py register 
 twine upload dist/gputools*
