9d9f228d638870270ebd484949ba7c351a07aa95
Martin Weigert <mweigert@mpi-cbg.de>
2017-01-11 21:02:38 +0100

    bump

--- setup.py
+++ setup.py
@@ -1,38 +1,51 @@
-
 from setuptools import setup, find_packages
 
-exec(open('gputools/version.py').read())
-
+exec (open('gputools/version.py').read())
 
 setup(name='gputools',
-    version=__version__,
-    description='OpenCL accelerated volume processing',
-    url='https://github.com/maweigert/gputools',
-    author='Martin Weigert',
-    author_email='mweigert@mpi-cbg.de',
-    license='BSD 3-Clause License',
-    packages=find_packages(),
+      version=__version__,
+      description='OpenCL accelerated volume processing',
+      url='https://github.com/maweigert/gputools',
+      author='Martin Weigert',
+      author_email='mweigert@mpi-cbg.de',
+      license='BSD 3-Clause License',
+      packages=find_packages(),
 
-    install_requires=["numpy>=1.11.0",
-                      "pyopencl>=2016.1",
-                      "configparser",
-                      "reikna>=0.6.7"],
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Science/Research',
+          'Topic :: Software Development :: Scientific/Engineering',
+          'License :: OSI Approved :: BSD License',
 
-    extras_require={
-    ':python_version<="3.0"': ["scikit-tensor",
-                               ],
-    },
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+      ],
+      keywords='science image-processing ',
 
-    package_data={"gputools":
-                  ['core/kernels/*.cl',
-                   'convolve/kernels/*.cl',
-                   'denoise/kernels/*.cl',
-                   'deconv/kernels/*.cl',
-                   'noise/kernels/*.cl',
-                   'transforms/kernels/*.cl',
-                  ],
+      install_requires=["numpy>=1.11.0",
+                        "pyopencl>=2016.1",
+                        "configparser",
+                        "reikna>=0.6.7"],
 
-    },
+      extras_require={
+          ':python_version<"3.0"': ["scikit-tensor",
+                                    "ConfigParser",
+                                    ],
+          ':python_version>="3.0"': ["configparser",
+                                     ],
+      },
 
-    entry_points = {}
-)
+      package_data={"gputools":
+                        ['core/kernels/*.cl',
+                         'convolve/kernels/*.cl',
+                         'denoise/kernels/*.cl',
+                         'deconv/kernels/*.cl',
+                         'noise/kernels/*.cl',
+                         'transforms/kernels/*.cl',
+                         ],
+
+                    },
+
+      entry_points={}
+      )
