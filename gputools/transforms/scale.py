f32be89cb1f6594f2135c2e8a669199e1a5b1eab
Martin Weigert <mweigert@mpi-cbg.de>
2017-10-11 02:18:29 +0200

    fixed bug in config. Started bicubic upsampling

--- gputools/transforms/scale.py
+++ gputools/transforms/scale.py
@@ -88,15 +88,58 @@ def scale(data, scale = (1.,1.,1.), interpolation = "linear"):
     return res_g.get()
 
 
+def scale_bicubic(data, scale=(1., 1., 1.)):
+    """
+    returns a interpolated, scaled version of data
 
+    the output shape is scaled too.
 
-if __name__ == '__main__':
-    
-    d = np.zeros((100,100,100),np.float32)
-    d[10:-10,10:-10,10:-10] = 1.
+    Parameters
+    ----------
+    data: ndarray
+        3d input array
+    scale: float, tuple
+        scaling factor along each axis (x,y,z) 
+    interpolation: str
+        either "nearest" or "linear"
 
-    res = scale(d,1.7, interp = "linear")
-    # res = scale(d)
+    Returns
+    -------
+        scaled output 
 
-    
-    
+    """
+
+    if not (isinstance(data, np.ndarray) and data.ndim == 3):
+        raise ValueError("input data has to be a 3d array!")
+
+    options_types = {np.uint8: ["-D", "TYPENAME=uchar", "-D", "READ_IMAGE=read_imageui"],
+                     np.uint16: ["-D", "TYPENAME=short", "-D", "READ_IMAGE=read_imageui"],
+                     np.float32: ["-D", "TYPENAME=float", "-D", "READ_IMAGE=read_imagef"],
+                     }
+
+    dtype = data.dtype.type
+
+    if not dtype in options_types:
+        raise ValueError("type %s not supported! Available: %s" % (dtype, str(list(options_types.keys()))))
+
+    if not isinstance(scale, (tuple, list, np.ndarray)):
+        scale = (scale,) * 3
+
+    if len(scale) != 3:
+        raise ValueError("scale = %s misformed" % scale)
+
+    d_im = OCLImage.from_array(data)
+
+    nshape = _scale_shape(data.shape, scale)
+
+    res_g = OCLArray.empty(nshape, dtype)
+
+    prog = OCLProgram(abspath("kernels/scale.cl"),
+                      build_options=options_types[dtype])
+
+    prog.run_kernel("scale_bicubic",
+                    res_g.shape[::-1], None,
+                    d_im, res_g.data)
+
+    return res_g.get()
+
