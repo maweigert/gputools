9d6435b15596feab4ad454c166663ac9a342aac0
Martin Weigert <marweigert@gmail.com>
2021-07-30 15:16:02 +0200

    Add strides parameter to gaussian_filter

--- gputools/convolve/convolve_sep.py
+++ gputools/convolve/convolve_sep.py
@@ -7,26 +7,28 @@ import os
 import numpy as np
 from gputools import OCLArray, OCLProgram, get_device
 
-from gputools.core.ocltypes import assert_bufs_type
+from gputools.core.ocltypes import assert_bufs_type, cl_buffer_datatype_dict
 from gputools.utils.tile_iterator import tile_iterator
 from gputools.separable import separable_series
 
 from ._abspath import abspath
+from .generic_separable_filters import _stride_shape
 
-
-def convolve_sep2(data, hx, hy, res_g=None, sub_blocks=None):
+def convolve_sep2(data, hx, hy, res_g=None, strides=1, sub_blocks=None):
     """convolves 2d data with kernel h = outer(hx,hy)
     boundary conditions are clamping to edge.
 
     data is either np array or a gpu buffer (OCLArray)
 
     """
+    if np.isscalar(strides):
+        strides = (strides,)*len(data.shape)
 
     if isinstance(data, np.ndarray):
         data = np.ascontiguousarray(data)
 
         if sub_blocks == (1, 1) or sub_blocks is None:
-            return _convolve_sep2_numpy(data, hx, hy)
+            return _convolve_sep2_numpy(data, hx, hy, strides=strides)
         else:
             # cut the image into tile and operate on every of them
             N_sub = [int(np.ceil(1. * n / s)) for n, s in zip(data.shape, sub_blocks)]
@@ -37,45 +39,56 @@ def convolve_sep2(data, hx, hy, res_g=None, sub_blocks=None):
                                      padsize=Npads,
                                      mode="constant"):
                 res_tile = _convolve_sep2_numpy(data_tile.copy(),
-                                                hx, hy)
+                                                hx, hy, strides=strides)
                 res[data_s_src] = res_tile[data_s_dest]
             return res
     elif isinstance(data, OCLArray):
         if not sub_blocks is None:
             raise NotImplementedError()
-        return _convolve_sep2_gpu(data, hx, hy, res_g=res_g)
+        return _convolve_sep2_gpu(data, hx, hy, res_g=res_g, strides=strides)
     else:
         raise TypeError("array argument (1) has bad type: %s" % type(data))
 
 
-def _convolve_sep2_numpy(data, hx, hy):
+def _convolve_sep2_numpy(data, hx, hy, strides=1):
     hx_g = OCLArray.from_array(hx.astype(np.float32))
     hy_g = OCLArray.from_array(hy.astype(np.float32))
 
     data_g = OCLArray.from_array(data.astype(np.float32))
 
-    return _convolve_sep2_gpu(data_g, hx_g, hy_g).get()
+    return _convolve_sep2_gpu(data_g, hx_g, hy_g, strides=strides).get()
 
+# the main gpu function in 2D
+def _convolve_sep2_gpu(data_g, hx_g, hy_g, strides, res_g=None):
+    assert_bufs_type(np.float32, hx_g, hy_g)
+    assert len(data_g.shape)==len(strides)==2
 
-def _convolve_sep2_gpu(data_g, hx_g, hy_g, res_g=None):
-    assert_bufs_type(np.float32, data_g, hx_g, hy_g)
+    prog = OCLProgram(abspath("kernels/convolve_sep.cl"), build_options=['-D','DTYPE=%s'%cl_buffer_datatype_dict[data_g.dtype.type]])
 
-    prog = OCLProgram(abspath("kernels/convolve_sep.cl"))
+    Ny,Nx = data_g.shape
+    Nhy, Nhx = hy_g.shape[0], hx_g.shape[0]
 
-    Ny, Nx = hy_g.shape[0], hx_g.shape[0]
+    out_shape_y = _stride_shape(data_g.shape, (strides[1], 1))
+    out_shape_x = _stride_shape(data_g.shape, (strides[0],strides[1]))
 
-    tmp_g = OCLArray.empty_like(data_g)
-
+    tmp_g = OCLArray.empty(out_shape_y, data_g.dtype)
+        
     if res_g is None:
-        res_g = OCLArray.empty_like(data_g)
-
-    prog.run_kernel("conv_sep2_x", data_g.shape[::-1], None, data_g.data, hx_g.data, tmp_g.data, np.int32(Nx))
-    prog.run_kernel("conv_sep2_y", data_g.shape[::-1], None, tmp_g.data, hy_g.data, res_g.data, np.int32(Ny))
+        res_g = OCLArray.empty(out_shape_x, data_g.dtype)
+    else:
+        assert res_g.shape==out_shape_x
+        
+    # here we perform first y and then x convolution
+    # this is importnant to ensure equality with scipy for integral types
+    prog.run_kernel("conv_sep2_y", out_shape_y[::-1], None, data_g.data, hy_g.data, tmp_g.data,
+                    np.int32(Nhy), np.int32(Ny), np.int32(strides[0]))
+    prog.run_kernel("conv_sep2_x", out_shape_x[::-1], None, tmp_g.data, hx_g.data, res_g.data,
+                    np.int32(Nhx), np.int32(Nx), np.int32(strides[1]))
 
     return res_g
 
 
-def convolve_sep3(data, hx, hy, hz, res_g=None, sub_blocks=(1, 1, 1), tmp_g = None):
+def convolve_sep3(data, hx, hy, hz, res_g=None, sub_blocks=(1, 1, 1), strides=1, tmp_g = None):
     """convolves 3d data with kernel h = outer(hx,hy, hz)
     boundary conditions are clamping to edge.
 
@@ -83,10 +96,13 @@ def convolve_sep3(data, hx, hy, hz, res_g=None, sub_blocks=(1, 1, 1), tmp_g = No
 
     """
 
+    if np.isscalar(strides):
+        strides = (strides,)*len(data.shape)
+    
     if isinstance(data, np.ndarray):
         data = np.ascontiguousarray(data)
         if sub_blocks == (1, 1, 1) or sub_blocks is None:
-            return _convolve_sep3_numpy(data, hx, hy, hz)
+            return _convolve_sep3_numpy(data, hx, hy, hz, strides=strides)
         else:
             # cut the image into tile and operate on every of them
             N_sub = [int(np.ceil(1. * n / s)) for n, s in zip(data.shape, sub_blocks)]
@@ -97,43 +113,59 @@ def convolve_sep3(data, hx, hy, hz, res_g=None, sub_blocks=(1, 1, 1), tmp_g = No
                                                padsize=Npads,
                                                mode="constant")):
                 res_tile = _convolve_sep3_numpy(data_tile.copy(),
-                                                hx, hy, hz)
+                                                hx, hy, hz, strides=strides)
                 res[data_s_src] = res_tile[data_s_dest]
             return res
 
 
     elif isinstance(data, OCLArray):
-        return _convolve_sep3_gpu(data, hx, hy, hz, res_g=res_g, tmp_g = tmp_g)
+        return _convolve_sep3_gpu(data, hx, hy, hz, res_g=res_g, tmp_g = tmp_g, strides=strides)
     else:
         raise TypeError("array argument (1) has bad type: %s" % type(data))
 
 
-def _convolve_sep3_numpy(data, hx, hy, hz):
+def _convolve_sep3_numpy(data, hx, hy, hz, strides):
     hx_g = OCLArray.from_array(hx.astype(np.float32))
     hy_g = OCLArray.from_array(hy.astype(np.float32))
     hz_g = OCLArray.from_array(hz.astype(np.float32))
 
     data_g = OCLArray.from_array(data.astype(np.float32))
 
-    return _convolve_sep3_gpu(data_g, hx_g, hy_g, hz_g).get()
+    return _convolve_sep3_gpu(data_g, hx_g, hy_g, hz_g, strides=strides).get()
 
 
-def _convolve_sep3_gpu(data_g, hx_g, hy_g, hz_g, res_g=None, tmp_g = None):
-    assert_bufs_type(np.float32, data_g, hx_g, hy_g)
+def _convolve_sep3_gpu(data_g, hx_g, hy_g, hz_g, strides, res_g=None, tmp_g = None):
+    assert_bufs_type(np.float32, hx_g, hy_g, hz_g)
 
-    prog = OCLProgram(abspath("kernels/convolve_sep.cl"))
+    prog = OCLProgram(abspath("kernels/convolve_sep.cl"), build_options=['-D','DTYPE=%s'%cl_buffer_datatype_dict[data_g.dtype.type]])
 
-    Nz, Ny, Nx = hz_g.shape[0], hy_g.shape[0], hx_g.shape[0]
+    Nz, Ny,Nx = data_g.shape
+    Nhz, Nhy, Nhx = hz_g.shape[0], hy_g.shape[0], hx_g.shape[0]
 
-    if tmp_g is None:
-        tmp_g = OCLArray.empty_like(data_g)
+    out_shape_z = _stride_shape(data_g.shape, (strides[0],1,1))
+    out_shape_y = _stride_shape(data_g.shape, (strides[0],strides[1],1))
+    out_shape_x = _stride_shape(data_g.shape, (strides[0],strides[1],strides[2]))
+
 
     if res_g is None:
-        res_g = OCLArray.empty_like(data_g)
+        res_g = OCLArray.empty(out_shape_x, data_g.dtype)
+    else:
+        assert res_g.shape==out_shape_x
 
-    prog.run_kernel("conv_sep3_x", data_g.shape[::-1], None, data_g.data, hx_g.data, res_g.data, np.int32(Nx))
-    prog.run_kernel("conv_sep3_y", data_g.shape[::-1], None, res_g.data, hy_g.data, tmp_g.data, np.int32(Ny))
-    prog.run_kernel("conv_sep3_z", data_g.shape[::-1], None, tmp_g.data, hz_g.data, res_g.data, np.int32(Nz))
+    if out_shape_z == out_shape_x:
+        tmp_g = res_g
+    else:
+        tmp_g = OCLArray.empty(out_shape_z, data_g.dtype)
+        
+    tmp2_g = OCLArray.empty(out_shape_y, data_g.dtype)
+    
+
+    prog.run_kernel("conv_sep3_z", out_shape_z[::-1], None, data_g.data, hz_g.data, tmp_g.data,
+                    np.int32(Nhz), np.int32(Nz), np.int32(strides[0]))
+    prog.run_kernel("conv_sep3_y", out_shape_y[::-1], None, tmp_g.data, hy_g.data, tmp2_g.data,
+                    np.int32(Nhy), np.int32(Ny), np.int32(strides[1]))
+    prog.run_kernel("conv_sep3_x", out_shape_x[::-1], None, tmp2_g.data, hx_g.data, res_g.data,
+                    np.int32(Nhx), np.int32(Nx), np.int32(strides[2]))
 
     return res_g
 
--- tests/convolve/test_gaussian.py
+++ tests/convolve/test_gaussian.py
@@ -4,30 +4,44 @@ import numpy.testing as npt
 import scipy.ndimage.filters as spf
 from itertools import combinations_with_replacement, combinations
 from gputools.convolve import gaussian_filter
+from gputools.convolve.generic_separable_filters import _gauss_filter
 
 
-def _test_single(dshape, sigma, dtype = np.float32, skip_assert = False):
-    x = np.random.randint(0, 40, dshape).astype(dtype)
+def _test_single(dshape, sigma, dtype = np.float32, strides=(1,1),skip_assert = False):
+    x = np.random.randint(0, 240, dshape).astype(dtype)
 
-    out1 = spf.gaussian_filter(x, sigma, mode= "constant", cval=0)
-    out2 = gaussian_filter(x, sigma)
+    ss_stride = tuple(slice(0,None,s) for s in strides)
+    
+    out1 = gaussian_filter(x, sigma, strides=strides)
+    out2 = spf.gaussian_filter(x, sigma, mode= "constant", cval=0)[ss_stride]
 
-    print(("shape: %s \tsigma: %s\ttype: %s\tdifference: %.2f" % (dshape, sigma, dtype,np.amax(np.abs(out1 - out2)))))
+    print(("shape: %s sigma: %s  strides %s  type: %s  diff: %.2f" % (dshape, sigma, strides, dtype,np.amax(np.abs(1.*out1 - out2)))))
     if not skip_assert:
-        npt.assert_almost_equal(out1,out2, decimal = 3)
+        npt.assert_almost_equal(out1,out2, decimal = 0)
     return out1, out2
 
 
 def test_all():
-    for ndim in [2,3]:
+    stridess = {2:((1,1),(2,2),(4,3)), 3:((1,1,1),(2,2,2),(4,1,1),(3,2,5))}
+    for ndim in (2,3):
         for dshape in combinations([19,31,43],ndim):
             for sigma in combinations_with_replacement([3,4,5],ndim):
-                for dtype in (np.float32,):
-                        _test_single(dshape,sigma, dtype = dtype)
+                for dtype in (np.float32,np.uint16, np.int32):
+                    for strides in stridess[ndim]:
+                        _test_single(dshape,sigma, dtype = dtype, strides=strides)
 
     
 if __name__ == '__main__':
-    x,y = _test_single((200,100),(3,4), dtype = np.float32)
+    # x,y = _test_single((10,10,10),(1,1,2), strides=(1,1,1), dtype = np.uint16, skip_assert=True)
+    np.random.seed(31)
+    x,y = _test_single((19, 31, 43),(3,3,0), strides=(1,1,1), dtype = np.uint16, skip_assert=True)
 
-   
+    # ind = np.unravel_index(np.argmax(np.abs(1.*x-y)), x.shape)
+    # print(ind)
 
+    # print(x[tuple(ind)])
+    # print(y[tuple(ind)])
+    
+
+    # from gputools import get_device
+    # get_device().queue.finish()
